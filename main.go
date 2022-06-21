e6e1f3cf7f2765dd374c888581e69b9b3ef73d91
Vachira J <vachira.jamsrijun@gmail.com>
2022-06-21 19:25:56 +0700

    testing packages

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/amoung-dev/hello-world/hello"
 	"github.com/amoung-dev/hello-world/world"
 )
 
+func displayGreetings(w io.Writer) {
+	fmt.Fprintln(w, hello.Greet())
+	fmt.Fprintln(w, world.Greet())
+}
+
 func main() {
-	fmt.Println(hello.Greet())
-	fmt.Println(world.Greet())
+	displayGreetings(os.Stdout)
 }
